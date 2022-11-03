1c9562bb4e86c9d2f9d9f4100a0caf2d032c7b89
Adam Macpherson <adam.ja.mac@gmail.com>
2022-11-03 13:56:15 +0000

    searchbar added

---
+++ src/Components/BrowserComponents/BrowserGrid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BrowserCard from './BrowserCard'
+
+function BrowserGrid() {
+  return 
+  ;<>
+    <div>BrowserGrid</div>
+    <BrowserCard></BrowserCard>
+  </>
+}
+
+export default BrowserGrid
