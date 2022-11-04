2bf1ffd7bbba4adb4dacb8ac8cad21dd616d7e4d
Helen Langridge <langridge.helen@outlook.com>
2022-11-04 11:33:01 +0000

    add organisation dropdown on signup

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,102 +1,152 @@
-import React, {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
-import {doc, setDoc, serverTimestamp} from 'firebase/firestore';
-import {db} from '../firebase.config';
-import "../Assets/index.css"
-import OAuth from "../Components/OAuth"
-import whitelogo from '../Assets/png/white-logo.png';
-import visibilityIcon from '../Assets/svg/visibilityIcon.svg';
-import {ReactComponent as ArrowRightIcon} from '../Assets/svg/keyboardArrowRightIcon.svg';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
+import "../Assets/index.css";
+import OAuth from "../Components/OAuth";
+import whitelogo from "../Assets/png/white-logo.png";
+import visibilityIcon from "../Assets/svg/visibilityIcon.svg";
+import { ReactComponent as ArrowRightIcon } from "../Assets/svg/keyboardArrowRightIcon.svg";
+import { toast } from "react-toastify";
 
 function SignUp() {
-
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: ''
-  })
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  const {name, email, password} = formData
-  const navigate = useNavigate()
+  const { name, email, password } = formData;
+  const navigate = useNavigate();
 
   const onChange = (e) => {
     setFormData((prevState) => ({
-      ...prevState, 
-      [e.target.id]: e.target.value
-    }))
-  }
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const auth = getAuth()
+      const auth = getAuth();
 
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-      const user = userCredential.user
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
 
       updateProfile(auth.currentUser, {
-        displayName: name
-      })
+        displayName: name,
+      });
 
-      const formDataCopy = {...formData}
-      delete formDataCopy.password
-      formDataCopy.timestamp = serverTimestamp()
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
 
-      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
-      navigate('/profile')
-    } catch (error){
+      navigate("/profile");
+    } catch (error) {
       const errorCode = error.code;
-      if (errorCode === 'auth/email-already-in-use') {
-        toast.error('Sorry, email already in use')
+      if (errorCode === "auth/email-already-in-use") {
+        toast.error("Sorry, email already in use");
       } else {
-        toast.error('Oops, something went wrong')
+        toast.error("Oops, something went wrong");
       }
     }
-  }
+  };
 
-  return (    
-  <div className='imageBackground'>
-    <header className='landingPageHeader'>
-    <a href="/"><img src={whitelogo} alt="VeloRepo" className="logo"/></a>
-      <div className="navLinks">
-        <Link className='landingPageLinks' to="/sign-up">Sign up</Link>
-        
-        <Link className='landingPageLinks' to="/sign-in">Log in</Link>
-      </div>
-    </header>
+  return (
+    <div className="imageBackground">
+      <header className="landingPageHeader">
+        <a href="/">
+          <img src={whitelogo} alt="VeloRepo" className="logo" />
+        </a>
+        <div className="navLinks">
+          <Link className="landingPageLinks" to="/sign-up">
+            Sign up
+          </Link>
 
-    <div className="signInBlock">
-      <div className="signInBox">
-        <div className="flexAlignCentreColumn">
-          <h2 className='textAlignCentre welcomeText'>Welcome to VeloRepo</h2>
-          <h3 className='textAlignCentre welcomeText'>Please enter your details to sign up</h3>
+          <Link className="landingPageLinks" to="/sign-in">
+            Log in
+          </Link>
         </div>
-        <form onSubmit={onSubmit}>
-        <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange}/>
-        <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange}/>
-          <div className="passwordInputDiv">
-            <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder="Password" id="password" value={password} onChange={onChange} minlength="6"/>
-            <img src={visibilityIcon} alt="show password" className="showPassword" onClick={() => setShowPassword((prevState) => !prevState)}/>
-        </div>
-        
-        <div className="flexAlignCenter">
-          <p className="signInText">Sign Up</p>
-            <button className="signInButton">
-            <ArrowRightIcon fill='#ffffff' width='34px' height='34px'/>
-            </button>
-        </div>
-        </form>
+      </header>
+      <div className=".mainContainer">
+      <div className="signInBlock">
+        <div className="signInBox">
+          <div className="flexAlignCentreColumn">
+            <h2 className="textAlignCentre welcomeText">Welcome to VeloRepo</h2>
+          </div>
+          <form onSubmit={onSubmit}>
+          <div>
+            <select name="organisation" id="organisation" onChange={onChange} className="orgInput">
+              <option value="">Please select your community</option>
+              <option value="Queens of Pain">Queens of Pain</option>
+              <option value="Lakes Gravel Gang">Lakes Gravel Gang</option>
+            </select>
+          </div>
 
-                <OAuth />
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+                minlength="6"
+              />
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            </div>
 
-        <Link to='/sign-in' className="registerLink">Already signed up? Log in instead</Link>
+            <div className="flexAlignCenter">
+              <p className="signInText">Sign Up</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          <OAuth />
+
+          <Link to="/sign-in" className="registerLink">
+            Already signed up? Log in instead
+          </Link>
+        </div>
       </div>
     </div>
     </div>
-  )
+  );
 }
-export default SignUp
+export default SignUp;
