a08996c2f949efc6ecc2c88cc81c0a7a3bcc4080
Helen Langridge <langridge.helen@outlook.com>
2022-11-09 12:46:32 +0000

    import tidy up

--- src/Pages/BrowsePage.js
+++ src/Pages/BrowsePage.js
@@ -1,69 +1,71 @@
 import React, { useState } from "react";
-import ReactDatePicker from "react-datepicker";
 import SearchBar from "../Components/BrowserComponents/SearchBar";
-import testRepo from "../Repositories/testRepository";
 import "react-datepicker/dist/react-datepicker.css";
 import "../Assets/browse.css";
 import BrowserGrid from "../Components/BrowserComponents/BrowserGrid";
 import BackEndHeader from "../Components/BackEndHeader";
-import DatePicker from "react-datepicker";
-import { DateTime } from "luxon";
 import { ReactUTCDatepicker } from "react-utc-datepicker";
 
-
-const BrowsePage = ({items}) => {
-
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchStartDate, setSearchStartDate] = useState('')
-  const [searchEndDate, setSearchEndDate] = useState('')
-
+const BrowsePage = ({ items }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchStartDate, setSearchStartDate] = useState("");
+  const [searchEndDate, setSearchEndDate] = useState("");
 
   const checkIfIsBookedDuringSearchParamas = (item) => {
-
-    for (let booking in item.bookings){
-      if (booking.startDate >= searchStartDate &&  booking.endDate <= searchEndDate) {
-        return false  
+    for (let booking in item.bookings) {
+      if (
+        booking.startDate >= searchStartDate &&
+        booking.endDate <= searchEndDate
+      ) {
+        return false;
       }
     }
-    return true 
-
-  }
+    return true;
+  };
 
   const filteredItems = items.filter((item) => {
-
-    if (searchTerm && searchStartDate && searchEndDate ) {
-      return item.name.toLowerCase().includes(searchTerm) && checkIfIsBookedDuringSearchParamas(item)
+    if (searchTerm && searchStartDate && searchEndDate) {
+      return (
+        item.name.toLowerCase().includes(searchTerm) &&
+        checkIfIsBookedDuringSearchParamas(item)
+      );
     }
     if (searchTerm && searchStartDate && item.bookings.length > 0) {
-      return item.name.toLowerCase().includes(searchTerm) && item.bookings[0].startDate >= searchStartDate
+      return (
+        item.name.toLowerCase().includes(searchTerm) &&
+        item.bookings[0].startDate >= searchStartDate
+      );
     }
 
     if (searchTerm && searchEndDate && item.bookings.length > 0) {
-      return item.name.toLowerCase().includes(searchTerm) && item.bookings[0].endDate <= searchEndDate
+      return (
+        item.name.toLowerCase().includes(searchTerm) &&
+        item.bookings[0].endDate <= searchEndDate
+      );
     }
 
     if (searchStartDate && searchEndDate && item.bookings.length > 0) {
-      return item.bookings[0].endDate <= searchEndDate && item.bookings[0].startDate >= searchStartDate
+      return (
+        item.bookings[0].endDate <= searchEndDate &&
+        item.bookings[0].startDate >= searchStartDate
+      );
     }
 
     if (searchTerm) {
-      return item.name.toLowerCase().includes(searchTerm)
+      return item.name.toLowerCase().includes(searchTerm);
     }
 
     if (searchStartDate && item.bookings.length > 0) {
-      return item.bookings[0].startDate >= searchStartDate
+      return item.bookings[0].startDate >= searchStartDate;
     }
 
     if (searchEndDate && item.bookings.length > 0) {
-      return item.bookings[0].endDate <= searchEndDate
+      return item.bookings[0].endDate <= searchEndDate;
     }
 
-    // Adds the item to the list if no filters are in place 
-    return true
-  })
-
-
-
+    // Adds the item to the list if no filters are in place
+    return true;
+  });
 
   // const filteredStartDates = items.filter((item) => {
   //   return item.bookings[0].startDate >= searchStartDate
@@ -73,26 +75,22 @@ const BrowsePage = ({items}) => {
   //   return item.bookings[0].endDate <= searchEndDate
   // })
 
-
   const updateSearchTerm = (searchTerm) => {
-    setSearchTerm(searchTerm.toLowerCase())
-  }
+    setSearchTerm(searchTerm.toLowerCase());
+  };
 
-
-  const browserGrid = <BrowserGrid
-
-    items={filteredItems}
-    searchStartDate={searchStartDate}
-    searchEndDate={searchEndDate}
-    className='BrowserGrid'
-  />
-
+  const browserGrid = (
+    <BrowserGrid
+      items={filteredItems}
+      searchStartDate={searchStartDate}
+      searchEndDate={searchEndDate}
+      className="BrowserGrid"
+    />
+  );
 
   const pageInformation = {
-    pageTitle: "Browse"
-
-  }
-
+    pageTitle: "Browse",
+  };
 
   return (
     <>
@@ -101,37 +99,41 @@ const BrowsePage = ({items}) => {
       <section className="datePickerAndSearch">
         <div className="datePickerLeft">
           <h2>From</h2>
-          <ReactUTCDatepicker format="YYYY-MM-DDT00:00:00.000+00:00" selected={searchStartDate} onChange={(date) => setSearchStartDate(date)} />
+          <ReactUTCDatepicker
+            format="YYYY-MM-DDT00:00:00.000+00:00"
+            selected={searchStartDate}
+            onChange={(date) => setSearchStartDate(date)}
+          />
           <h2>To</h2>
-          <ReactUTCDatepicker format="YYYY-MM-DDT00:00:00.000+00:00" selected={searchEndDate} onChange={(date) => setSearchEndDate(date)} />
+          <ReactUTCDatepicker
+            format="YYYY-MM-DDT00:00:00.000+00:00"
+            selected={searchEndDate}
+            onChange={(date) => setSearchEndDate(date)}
+          />
         </div>
 
-        <div className='SearchBarRight'>
+        <div className="SearchBarRight">
           <SearchBar handleChange={updateSearchTerm} searchTerm={searchTerm} />
         </div>
       </section>
 
-
       <main className="mainContainer">
-        <div className="browserGrid">
-          {browserGrid}
-        </div>
+        <div className="browserGrid">{browserGrid}</div>
       </main>
     </>
   );
-}
+};
 
 export default BrowsePage;
 
+// const browserGridWithFilteredStartDates = <BrowserGrid
 
-  // const browserGridWithFilteredStartDates = <BrowserGrid
+//   items={filteredStartDates}
+//   className='BrowserGrid'
+// />
 
-  //   items={filteredStartDates}
-  //   className='BrowserGrid'
-  // />
+// const browserGridWithFilteredEndDates = <BrowserGrid
 
-  // const browserGridWithFilteredEndDates = <BrowserGrid
-
-  //   items={filteredEndDates}
-  //   className='BrowserGrid'
-  // />
+//   items={filteredEndDates}
+//   className='BrowserGrid'
+// />
